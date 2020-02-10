660aa921109b9bfc471313ba381038d0ce9d8756
raghu <raghu@LAPTOP-ATI2NALF.cable.rcn.com>
2020-02-10 01:02:16 -0600

    Lab 4 - Initial Commit

---
+++ src/main/java/edu/iit/sat/itmd4515/rbalasubramanian1/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package edu.iit.sat.itmd4515.rbalasubramanian1.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
